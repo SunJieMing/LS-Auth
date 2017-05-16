16a77c386c1c64b885de45bb0c1f0209e4e76f33
sunjieming <ben@lambdaschool.com>
2017-05-16 13:27:21 -0600

    Add cors

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/auth');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 require('./controllers')(app);
 
